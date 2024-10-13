dbac7978935540438d98e5b1961b8cb9d7038be7
niroe5tar64 <niroe5tar64@gmail.com>
2024-10-13 16:23:58 +0900

    chore: プロジェクト名を[niroreal-gas-libraries]に変更し、サンプル定義を整理

--- public/about_public.js
+++
@@ -1,5 +0,0 @@
-// files in public folder are output as is in dist folder
-
-function functionFromPublic() {
-  console.log("functionFromPublic");
-} // functionFromPublic
--- src/module-sample.ts
+++
@@ -1,4 +0,0 @@
-// Sample module
-const calc = (a: number, b: number) => a + b;
-
-export { calc };
